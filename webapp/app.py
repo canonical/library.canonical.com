42dc60d89bbbb3b2d616e3d0dc22c5c209c3b5a9
petesfrench <peterfrench94@gmail.com>
2023-04-13 10:57:03 +0200

    Make side-nav persist across pages and url to be human readable

--- webapp/app.py
+++ webapp/app.py
@@ -21,13 +21,30 @@ session = talisker.requests.get_session()
 
 @app.route("/")
 def index():
-    navigation = Navigation(drive).hierarchy
-
-    return flask.render_template("index.html", navigation=navigation)
-
-
-@app.route("/document/<document_id>")
-def document(document_id):
+    navigation = Navigation(drive)
+    document_id = navigation.hierarchy["home"]["id"]
     soup = Parser(drive, document_id)
 
-    return flask.render_template("document.html", document=soup.html)
+    return flask.render_template(
+        "index.html", navigation=navigation.hierarchy, document=soup.html
+    )
+
+
+@app.route("/<path:path>")
+def document(path):
+    navigation = Navigation(drive)
+    document_id = get_page_id(path, navigation.hierarchy)
+    soup = Parser(drive, document_id)
+
+    return flask.render_template(
+        "index.html", navigation=navigation.hierarchy, document=soup.html
+    )
+
+
+def get_page_id(path, navigation):
+    split_slug = path.split("/")
+    target_page = navigation
+    for index, slug in enumerate(split_slug):
+        if len(split_slug) == index + 1:
+            return target_page[slug]["id"]
+        target_page = target_page[slug]["children"]
--- webapp/navigation.py
+++ webapp/navigation.py
@@ -11,22 +11,21 @@ class Navigation:
 
     def create_hierarchy(self, objects):
         object_dict = {}
-        root_objects = []
+        root_objects = {}
 
-        # create a temp storage for each item and append children prop
         for obj in objects:
-            obj["children"] = []
+            obj["children"] = {}
             obj["mimeType"] = obj["mimeType"].rpartition(".")[-1]
+            obj["slug"] = "-".join(obj["name"].split(" ")).lower()
             object_dict[obj["id"]] = obj
 
-        # iterate through temp storage to create parent-child relationships
         for obj in objects:
             parent_ids = obj["parents"]
             for parent_id in parent_ids:
                 parent_obj = object_dict.get(parent_id)
                 if parent_obj is not None:
-                    parent_obj["children"].append(obj)
+                    parent_obj["children"][obj["slug"]] = obj
                 else:
-                    root_objects.append(obj)
+                    root_objects[obj["slug"]] = obj
 
         return root_objects
