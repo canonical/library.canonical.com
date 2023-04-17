0bcb582417badd80fe6d5736a36d286721084275
petesfrench <peterfrench94@gmail.com>
2023-04-17 14:28:20 +0200

    Create parse_links function

--- webapp/navigation.py
+++ webapp/navigation.py
@@ -5,10 +5,17 @@ class Navigation:
     def __init__(self, google_drive: Drive):
         file_list = google_drive.get_document_list()
         self.hierarchy = self.create_hierarchy(file_list)
+        self.object_dict
 
     def create_hierarchy(self, objects):
-        object_dict = {}
+        self.object_dict = {}
         root_objects = {}
+
+        def add_full_path(obj, path=""):
+            for key in obj.keys():
+                obj[key]["full_path"] = path + "/" + obj[key]["slug"]
+                if obj[key]["mimeType"] == "folder":
+                    add_full_path(obj[key]["children"], obj[key]["full_path"])
 
         for obj in objects:
             obj["children"] = {}
@@ -16,15 +23,18 @@ class Navigation:
             obj["slug"] = "-".join(obj["name"].split(" ")).lower()
             obj["active"] = False
             obj["expanded"] = False
-            object_dict[obj["id"]] = obj
+            obj["path"] = ""
+            self.object_dict[obj["id"]] = obj
 
         for obj in objects:
             parent_ids = obj["parents"]
             for parent_id in parent_ids:
-                parent_obj = object_dict.get(parent_id)
+                parent_obj = self.object_dict.get(parent_id)
                 if parent_obj is not None:
                     parent_obj["children"][obj["slug"]] = obj
                 else:
                     root_objects[obj["slug"]] = obj
 
-        return root_objects
+        add_full_path(root_objects)
+
+        return root_objects["library"]["children"]
