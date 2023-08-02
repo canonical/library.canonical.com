15473708ac0266cf78e887e2ffc56390025b7292
Pete <peterfrench94@gmail.com>
2023-08-02 09:21:51 +0100

    Setup unittest and write ba sic connection tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,31 +1,37 @@
 import unittest
-from webapp.app import app
 
+from webapp.app import app
 
 class TestRoutes(unittest.TestCase):
     def setUp(self):
         """
-        Set up Flask app for testing
+        Setup Flask app for testing
         """
         app.testing = True
-        self.client = app.test_client()
+        self.app = app.test_client()
+        with self.app.session_transaction() as sess:
+            sess["openid"] = {
+                "identity_url": "localhost",
+                "email": "testing@ubuntu.com",
+                "fullname": "Test user",
+            }
 
-    def test_homepage(self):
+    def test_home_page(self):
         """
         When given the index URL,
         we should return a 200 status code
         """
-
-        self.assertEqual(self.client.get("/").status_code, 200)
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
 
     def test_not_found(self):
         """
         When given a non-existent URL,
         we should return a 404 status code
         """
+        response = self.app.get('/not-found-url')
+        self.assertEqual(response.status_code, 404)
 
-        self.assertEqual(self.client.get("/not-found-url").status_code, 404)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
