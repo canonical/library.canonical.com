ed97a37fbd60f66169b44c74dd7fe6787c9439ba
Nicolas Bello <nicolas.bello@canonical.com>
2024-11-19 14:50:38 -0500

    fix: solved comments in PR

--- sideNav/src/components/folder/folder.tsx
+++ sideNav/src/components/folder/folder.tsx
@@ -4,6 +4,7 @@ import { ExpandMore, ChevronRight} from '@mui/icons-material';
 import { Document, MAX_NUMBER_LEVELS } from '../sidebar/sidebar';
 import './folder.css';
 import { levelDocument } from '../sidebar/sidebar';
+import { sortChildren } from './parentFolder';
 
 interface FolderProps {
     document: Document;
@@ -58,17 +59,8 @@ const Folder: React.FC<FolderProps> = ({
         const processChildren = Object.keys(doc.children).map((key) => doc.children[key]);
         doc.postChildren = processChildren;
         return doc.postChildren.sort((a,b) => {
-            if (a.position === null && b.position === null) {
-                return a.name.localeCompare(b.name);
-              }
-              if (a.position === null && b.position !== null) {
-                return 1;
-              }
-              if (a.position !== null && b.position === null) {
-                return -1;
-              }
-              return (a.position ?? 0) - (b.position ?? 0);
-          }).map((doc) => {
+            return sortChildren(a,b);
+        }).map((doc) => {
             if(doc.name !== 'index'){
                 return <Folder
                         document={doc}
@@ -206,8 +198,8 @@ const Folder: React.FC<FolderProps> = ({
             : <ChevronRight/>}
             </div>
             :null}
-            <a href={document.full_path} style={{textDecoration:'none'}}>
-                <p className='navigation__folder-tittle' onClick={() => handleFolderClick(document)}>{document.name}</p>
+            <a href={document.full_path} className='navigation__link'>
+                <span className='navigation__folder-tittle' onClick={() => handleFolderClick(document)}>{document.name}</span>
             </a>
         </div>
         }
--- sideNav/src/components/folder/parentFolder.tsx
+++ sideNav/src/components/folder/parentFolder.tsx
@@ -22,6 +22,19 @@ interface position {
     x: number;
     y: number;
 }
+
+export function sortChildren(a: Document, b: Document): number {
+    if (a.position === null && b.position === null) {
+        return a.name.localeCompare(b.name);
+    }
+    if (a.position === null && b.position !== null) {
+        return 1;
+    }
+    if (a.position !== null && b.position === null) {
+        return -1;
+    }
+    return (a.position ?? 0) - (b.position ?? 0);
+}
   
 const ParentFolder: React.FC<ParentFolderProps> = ({ 
     document,
@@ -61,17 +74,8 @@ const ParentFolder: React.FC<ParentFolderProps> = ({
         const processChildren = Object.keys(doc.children).map((key) => doc.children[key]);
         doc.postChildren = processChildren;
         return doc.postChildren.sort((a,b) => {
-            if (a.position === null && b.position === null) {
-                return a.name.localeCompare(b.name);
-              }
-              if (a.position === null && b.position !== null) {
-                return 1;
-              }
-              if (a.position !== null && b.position === null) {
-                return -1;
-              }
-              return (a.position ?? 0) - (b.position ?? 0);
-          }).map((doc) => {
+            return sortChildren(a,b);
+        }).map((doc) => {
             if(doc.name !== 'index'){
                 return <Folder 
                         document={doc}
@@ -253,8 +257,8 @@ const ParentFolder: React.FC<ParentFolderProps> = ({
             : <ChevronRight/>}
             </div> 
             : null}
-            <a href={document.full_path} style={{textDecoration:'none'}}>
-                <p className='navigation__folder-tittle' onClick={() => handleFolderClick(document)}>{document.name}</p>
+            <a href={document.full_path} className='navigation__link'>
+                <span className='navigation__folder-tittle' onClick={() => handleFolderClick(document)}>{document.name}</span>
             </a>
         </div>
         }
