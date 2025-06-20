d43887c5674952626cfd2731b3edb3074c0bd5d4
nbellol <nbellolugo97@gmail.com>
2025-06-20 10:25:02 -0500

    fix: requirements and test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -36,7 +36,7 @@ class TestRoutes(unittest.TestCase):
         When given the index URL,
         we should return a 200 status code
         """
-        response = self.app.get("https://library.canonical.com/")
+        response = self.app.get("/")
         print(response)
         self.assertEqual(response.status_code, 200)
 
